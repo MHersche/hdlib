7a89d826894ba0000de4e754cc01288a9e0224a3
Michael Hersche <herschmi@iis.ee.ethz.ch>
2019-05-23 15:13:41 +0200

    Language Classification first test run

--- examples/language_classif/load_data.py
+++ examples/language_classif/load_data.py
@@ -15,26 +15,34 @@ __date__ = "20.5.2019"
 import time, sys, glob
 import numpy as np
 
-from hd_classifier import hd_classifier
 
 data_dir = 'data/'
 
-class Dataset:
+class load_data:
 
 	def __init__(self): 
 	
-	# training files 
-	self._langLabels = {'afr', 'bul', 'ces', 'dan', 'nld', 'deu', 'eng', 'est', 'fin', 'fra', 'ell', 'hun', 'ita', 'lav', 'lit', 'pol', 'por', 'ron', 'slk', 'slv', 'spa', 'swe'}
-	self._n_labels = len(self._langLabels)
-	self._tr_idx = 0
-	self._tr_path = data_dir + 'training_texts/'
+		# training files 
+		self._langLabels = {0:'afr', 1:'bul', 2:'ces', 3:'dan', 4:'nld', 
+		5:'deu', 6:'eng', 7:'est', 8:'fin', 9:'fra', 10:'ell', 11:'hun', 
+		12:'ita', 13:'lav', 14:'lit', 15:'pol', 16:'por', 17:'ron', 
+		18:'slk', 19:'slv', 19:'spa', 20:'swe'}
+		_test_langLabels = {0:'af', 1:'bg', 2:'cs', 3:'da', 4:'nl', 
+			5:'de', 6:'en', 7:'et', 8:'fi', 9:'fr', 10:'el', 11:'hu', 
+			12:'it', 13:'lv', 14:'lt', 15:'pl', 16:'pt', 17:'ro', 
+			18:'sk', 19:'sl', 19:'es', 20:'sv'}
+		self._test_langLabels = dict([(value, key) for key, value in _test_langLabels.items()]) 
 
-	# testing files 
-	self._testList = glob.glob(data_dir+"/testing_texts/*.txt")
-	self._n_test_labels = len(self._testList)
-	self._test_idx = 0
+		self._n_labels = len(self._langLabels)
+		self._tr_idx = 0
+		self._tr_path = data_dir + 'training_texts/'
 
-	return 
+		# testing files 
+		self._testList = glob.glob(data_dir+"testing_texts/*.txt")
+		self._n_test_labels = len(self._testList)
+		self._test_idx = 0
+
+		return 
 
 
 	def get_train_item(self):
@@ -49,25 +57,36 @@ class Dataset:
 		'''
 
 		if self._tr_idx < self._n_labels-1: 
-			F = open(self._tr_path+self._langLabels[_tr_idx])
+			fname=self._tr_path+self._langLabels[self._tr_idx]+'.txt'
+			F = open(fname)
 			string = F.read()
-			char_array = np.array(list(string))
+			char_array = np.array(list(string),'c')
 			F.close()
 			self._tr_idx +=1
 		else: 
 			self._tr_idx = 0
-			char_array = []
-
-		return char_array, self._tr_idx-1
+			char_array = np.array((100,))
+		return char_array.view(np.uint8), np.array(self._tr_idx-1)
 
 	def get_test_item(self):
+		if self._test_idx < self._n_test_labels:
+			fname = self._testList[self._test_idx]
+			last_slash = fname.rfind('/')
+			label =self._test_langLabels[fname[last_slash+1:last_slash+3]]
+			 
+			F = open(fname)
+			string = F.read()
+			char_array = np.array(list(string),'c')
+			F.close()
+			self._test_idx +=1
 
-		F = open(self._tr_path+self._langLabels[_tr_idx])
-		string = F.read()
-		char_array = np.array(list(string))
-		F.close()
+		else:
+			char_array = np.array([])
+			label = -1
+			
 
-		return char_array, self._tr_idx
+
+		return char_array.view(np.uint8), np.array(label)
 
 
 
--- examples/language_classif/plot_language_class.py
+++ examples/language_classif/plot_language_class.py
@@ -14,6 +14,7 @@ __date__ = "20.5.2019"
 
 import time, sys 
 import torch as t 
+import numpy as np
 
 
 sys.path.append('../../pyhdlib/')
@@ -22,31 +23,79 @@ from load_data import load_data
 
 
 
-
+training = True
 # data loader 
 dl = load_data()
 
 # init HD classifier 
 ngramm = 3
 encoding = "sumNgramm"
-nitem = 40
-D = 1000
+nitem = 256
+D = 5000
+device = 'cuda:0'
 
 
-hd = hd_classifier(D,encoding,nitem,ngramm)
+name = 'data/models/3gramm'
 
 
-# training
-hd.am_init(dl._n_labels)
-data,label = dl.get_train_item() 
-data_t = t.from_numpy(data).view(1,-1)
-label_t= t.from_numpy(data).view(1,-1)
-while (label != -1) : 
+hd = hd_classifier(D,encoding,device,nitem,ngramm,name=name)
 
-	hd.am_update(data_t)
-	data,label = dl.get_train_item() 
-	data_t = t.from_numpy(data).view(1,-1)
-	label_t= t.from_numpy(data).view(1,-1)
+
+
+########################## training ########################################
+
+if training: 
+	hd.am_init(dl._n_labels)
+	label = 0
+	
+	while (label != -1) : 
+		# load data 
+		data,label = dl.get_train_item() 
+	
+		if label == -1: 
+			break 
+		
+		data_t = t.from_numpy(data).view(1,-1).type(t.LongTensor).to(hd._device)
+		label_t= t.from_numpy(label).view(-1).type(t.LongTensor).to(hd._device)
+		print("train class {:} ".format(dl._langLabels[np.asscalar(label)]))
+		# train am 
+		hd.am_update(data_t,label_t)
+	
+	hd.am_threshold()
+	hd.save()
+
+
+########################## testing ########################################
+err = 0
+
+
+while (False): 
+	# get data and push to gpu 
+	X,y = dl.get_test_item()
+	X_t = t.from_numpy(X).view(1,-1).type(t.LongTensor).to(hd._device)
+	y_t= t.from_numpy(y).view(-1).type(t.FloatTensor).to(hd._device)
+	# estimation 
+
+	y_hat = hd.predict(X_t)
+	
+	curr_err = t.sum(y_hat != y_t)
+	err += curr_err
+	if curr_err != 0:
+		print("Error: True class: {:}, Estimation: {:}".format(y_t,y_hat))
+	
+	
+
+
+
+	
+	 
+
+	
+	
+
+
+
+
 
 
 
